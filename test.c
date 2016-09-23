d9c5ec77e578c839c28d1b08cd3a9c1310ea6f16
jeremow <jeremy.hraman@laposte.net>
2016-09-23 13:43:53 +0200

    Add files via upload

---
+++ test.c
@@ -0,0 +1,22 @@
+//
+//  test.c
+//  
+//
+//  Created by Jérémy Hraman on 18/09/2016.
+//
+//
+
+#include "test.h"
+
+#include <ctype.h>
+
+#include <readline/readline.h>
+#include <readline/history.h>
+
+
+int main() {
+    char* a = "ar4 5";
+    char first = a[2];
+    if ()
+        printf("%c \n",first);
+}
