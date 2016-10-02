68b65621940c1e8c44447049637f800a2a2e038b
romain.wg@live.fr <wgromain@localhost.localdomain>
2016-10-03 00:45:58 +0200

    Ajout de print.c et de print.h

--- scheme/include/print.h
+++ scheme/include/print.h
@@ -19,9 +19,20 @@ extern "C" {
 
 
 void sfs_print( object );
+//La fonction print est le point d’entrée de l’affichage du résultat de votre interpréteur.
+//Son rôle est de convertir un arbre syntaxique en chaîne de caractères et de l’afficher dans la sortie standard
+
+//SI type == SFS_PAIR (3) -> print pair
+//ELSE -> print atom
+
 
 void sfs_print_atom( object );
+//Conversion et écriture de l'atome
+//Switch des types et écriture en conséquence
+//SI liste vide : écriture d'une parenthèse fermante
+
 void sfs_print_pair( object );
+//écriture d'une parenthèse ouvrante
 
 #ifdef __cplusplus
 }
--- scheme/src/print.c
+++ scheme/src/print.c
@@ -12,46 +12,57 @@
 
 #include <stdio.h>
 
-void sfs_print_atom( object o ) {
+#include <string.h>
+
+void sfs_print_atom( object obj ) {
     
-    switch (o->type) {
-        case SFS_NUMBER :
-            printf("%d",o->this.number.this.integer);
-            
-        case SFS_BOOLEAN :
-            printf("%s",o->this.symbol);
-            
-        case SFS_CHARACTER :
-            printf("%c",o->this.character);
-            
-        case SFS_STRING :
-            printf("%s",o->this.string);
-            
-        case SFS_SYMBOL :
-            printf("%s",o->this.symbol);
-    }
-    
-    return;
+	switch ()
+	{
+		case SFS_NUMBER:
+			printf("%d",obj->this.number.this.integer);
+			break;
+		case SFS_CHARACTER:
+			printf("%c",obj->this.character);
+			break;
+		case SFS_STRING:
+			printf("%s",obj->this.string);
+			break;
+		case SFS_BOOLEAN:
+			printf("%s",obj->this.symbol);
+			break;
+		case SFS_SYMBOL:
+			printf("%s",obj->this.symbol);
+			break;
+		case SFS_NIL:
+			//écriture de la parenthèse fermante en cas de liste vide
+			printf(")");
+			break;		
+	}
+
 }
 
-void sfs_print_pair( object o ) {
-    
-    sfs_print( (o->this.pair).car );
-    sfs_print( (o->this.pair).cdr );
+void sfs_print_pair( object obj ) {
+	
+	//écriture de la parenthèse ouvrante
+	printf("(");
+	
+	//print de l'objet contenue à gauche puis à droite de l'arbre
+	sfs_print(obj->this.pair.car);
+	sfs_print(obj->this.pair.cdr);
 
-    return;
 }
 
-void sfs_print( object o ) {
-
-    if ( SFS_PAIR == o->type ) {
-        
-        sfs_print_pair( o );
-    }
-    
-    else {
-        
-        sfs_print_atom( o );
-    }
+void sfs_print( object obj ) {
+	
+	//l'object est une pair : print_pair
+	//sinon print_atom
+	if(obj->this.type == SFS_PAIR)
+	{
+		sfs_print_pair(obj);
+	}
+	else
+	{
+		sfs_print_atom(obj);
+	}
 
 }
