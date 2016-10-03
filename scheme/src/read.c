f04f430faaecfae5973f6879dccad2111a861e9c
jeremow <jeremy.hraman@laposte.net>
2016-10-03 14:19:51 +0200

    Add files via upload

--- scheme/src/read.c
+++ scheme/src/read.c
@@ -1,4 +1,3 @@
-
 /**
  * @file read.c
  * @author François Cayre <cayre@yiking.(null)>
@@ -12,11 +11,12 @@
 #include <ctype.h>
 
 #include "read.h"
-
+#include "read_atom.h"
+#include "aux_read.h"
 
 
 void flip( uint *i ) {
-
+    
     if ( *i == FALSE ) {
         *i = TRUE;
     }
@@ -32,7 +32,7 @@ void flip( uint *i ) {
  * ou NULL si line ne contient que des espaces et des commentaires
  */
 char* first_usefull_char(char* line) {
-
+    
     int i=0;
     if (line == NULL) {
         return NULL;
@@ -70,7 +70,7 @@ char* first_usefull_char(char* line) {
  * Les parentheses dans des chaines et les caracteres Scheme #\( et #\)
  * ne sont pas comptes.
  *
- * Si le compte devient zéro et que 
+ * Si le compte devient zéro et que
  *        - la ligne est fini, la fonction retourne S_OK
  * 				- la ligne n'est pas fini la fonction retourne S_KO
  *
@@ -109,48 +109,48 @@ uint  sfs_get_sexpr( char *input, FILE *fp ) {
     char      *chunk = NULL;
     uint      i = 0;
     string    sfs_prompt;
-
+    
     EXPRESSION_TYPE_T typeOfExpressionFound = NOTHING;
-
+    
     parlevel = 0;
     memset( input, '\0', BIGSTRING );
-
+    
     do {
         ret = NULL;
         chunk=k;
         memset( chunk, '\0', BIGSTRING );
-
+        
         /* si en mode interactif*/
         if ( stdin == fp ) {
             uint nspaces = 2*parlevel;
-
+            
             init_string( sfs_prompt );
-
+            
             /* le prompt indique le niveau de parenthese
-               et decale la prochaine entrée en fonction
-               de ce niveau (un peu à la python)*/
+             et decale la prochaine entrée en fonction
+             de ce niveau (un peu à la python)*/
             sprintf( sfs_prompt, "SFS:%u > ", parlevel );
-
+            
             for ( i= 0; i< nspaces; i++ ) {
                 sfs_prompt[strlen(sfs_prompt)] = ' ';
             }
-
+            
             /* si sur plusieurs lignes, le \n équivaut à un espace*/
             if (nspaces>0) {
                 input[strlen(input)+1] = '\0';
                 input[strlen(input)] = ' ';
             }
-
+            
             /*saisie de la prochaine ligne à ajouter dans l'input*/
             printf("%s",sfs_prompt);
             ret = fgets( chunk, BIGSTRING, fp );
             if (ret && chunk[strlen(chunk)-1] == '\n') chunk[strlen(chunk)-1] = '\0';
-
+            
         }
         /*si en mode fichier*/
         else {
             ret = fgets( chunk, BIGSTRING, fp );
-
+            
             if ( NULL == ret ) {
                 /* fin de fichier...*/
                 if ( parlevel != 0 ) {
@@ -159,95 +159,95 @@ uint  sfs_get_sexpr( char *input, FILE *fp ) {
                 }
                 return S_END;
             }
-
+            
             if (strlen(chunk) == BIGSTRING-1
-                    && chunk[BIGSTRING-1] != '\n'
-                    && !feof(fp)) {
+                && chunk[BIGSTRING-1] != '\n'
+                && !feof(fp)) {
                 WARNING_MSG( "Too long line for this interpreter!" );
                 return S_KO;
             }
         }
-
-        /* si la ligne est inutile 
+        
+        /* si la ligne est inutile
         	=> on va directement à la prochaine iteration */
         if (first_usefull_char(chunk) == NULL) {
             continue;
         }
-
-
+        
+        
         s = strlen( chunk );
-
+        
         if ( s > 0 ) {
             if (strlen(input) + s > BIGSTRING-1 ) {
                 WARNING_MSG( "Too long a S-expression for this interpreter!" );
                 return S_KO;
             }
-
+            
             for ( i = 0; i< strlen(chunk); i++ ) {
                 /* si la fin de la ligne chunk est inutile,
-                   on ajoute un espace dans input et on sort de la boucle*/
+                 on ajoute un espace dans input et on sort de la boucle*/
                 if ( in_string == FALSE && first_usefull_char(chunk + i) == NULL ) {
                     chunk[i]='\0';
                     input[strlen(input)] = ' ';
                     break;
                 }
-
-
+                
+                
                 switch(chunk[i]) {
-                case '(':
-                    if (in_string == FALSE
+                    case '(':
+                        if (in_string == FALSE
                             && ! ( i>1 && chunk[i-1] == '\\' && chunk[i-2] == '#' ) ) {
-                        parlevel++;
-                        typeOfExpressionFound = S_EXPR_PARENTHESIS;
-                    }
-                    break;
-                case ')':
-                    if ( in_string == FALSE
+                            parlevel++;
+                            typeOfExpressionFound = S_EXPR_PARENTHESIS;
+                        }
+                        break;
+                    case ')':
+                        if ( in_string == FALSE
                             && ! ( i>1 && chunk[i-1] == '\\' && chunk[i-2] == '#' ) ) {
-                        parlevel--;
-                        if (parlevel == 0 ) {
-                            typeOfExpressionFound = FINISHED;
-                        }
-                        if ( parlevel < 0 ) {
-                            WARNING_MSG( "Parse error : cannot start with ')'" );
-                            return S_KO;
-                        }
-                    }
-                    break;
-                case '"':
-                    if ( i<2 || chunk[i-1] != '\\' ) {
-                        if ( in_string == FALSE ) {
-                            if(typeOfExpressionFound == BASIC_ATOME) {
-                                WARNING_MSG("Parse error: invalid string after atom : '%s'", chunk+i);
+                            parlevel--;
+                            if (parlevel == 0 ) {
+                                typeOfExpressionFound = FINISHED;
+                            }
+                            if ( parlevel < 0 ) {
+                                WARNING_MSG( "Parse error : cannot start with ')'" );
                                 return S_KO;
                             }
-                            in_string = TRUE;
-                            if(typeOfExpressionFound != S_EXPR_PARENTHESIS) {
-                                typeOfExpressionFound = STRING_ATOME;
+                        }
+                        break;
+                    case '"':
+                        if ( i<2 || chunk[i-1] != '\\' ) {
+                            if ( in_string == FALSE ) {
+                                if(typeOfExpressionFound == BASIC_ATOME) {
+                                    WARNING_MSG("Parse error: invalid string after atom : '%s'", chunk+i);
+                                    return S_KO;
+                                }
+                                in_string = TRUE;
+                                if(typeOfExpressionFound != S_EXPR_PARENTHESIS) {
+                                    typeOfExpressionFound = STRING_ATOME;
+                                }
+                            }
+                            else {
+                                in_string = FALSE;
+                                if(typeOfExpressionFound == STRING_ATOME) {
+                                    typeOfExpressionFound = FINISHED;
+                                }
                             }
                         }
-                        else {
-                            in_string = FALSE;
-                            if(typeOfExpressionFound == STRING_ATOME) {
-                                typeOfExpressionFound = FINISHED;
+                        break;
+                    default:
+                        if(in_string == FALSE) {
+                            if(isspace(chunk[i])) {
+                                if(typeOfExpressionFound == BASIC_ATOME) {
+                                    typeOfExpressionFound = FINISHED;
+                                }
+                            } else if(typeOfExpressionFound != S_EXPR_PARENTHESIS) {
+                                typeOfExpressionFound = BASIC_ATOME;
                             }
                         }
-                    }
-                    break;
-                default:
-                    if(in_string == FALSE) {
-                        if(isspace(chunk[i])) {
-                            if(typeOfExpressionFound == BASIC_ATOME) {
-                                typeOfExpressionFound = FINISHED;
-                            }
-                        } else if(typeOfExpressionFound != S_EXPR_PARENTHESIS) {
-                            typeOfExpressionFound = BASIC_ATOME;
-                        }
-                    }
-                    break;
+                        break;
                 }
-
-
+                
+                
                 if(typeOfExpressionFound == FINISHED) {
                     char *first_useful = first_usefull_char(chunk + i + 1);
                     if( first_useful != NULL) {
@@ -260,7 +260,7 @@ uint  sfs_get_sexpr( char *input, FILE *fp ) {
                         return S_KO;
                     }
                 }
-
+                
                 /* recopie char par char*/
                 input[strlen(input)] = chunk[i];
             }
@@ -269,21 +269,21 @@ uint  sfs_get_sexpr( char *input, FILE *fp ) {
                 return S_KO;
             }
         }
-
-
+        
+        
         if ( parlevel > 0 && fp != stdin ) {
             if ( feof( fp ) ) {
                 WARNING_MSG( "Parse error: missing ')'" );
                 return S_KO;
             }
-
+            
             if (input[strlen(input)-1] == '\n') input[strlen(input)-1] = ' ';
         }
     } while ( parlevel > 0 );
-
+    
     /* Suppression des espaces restant a la fin de l'expression, notamment le dernier '\n' */
     while (isspace(input[strlen(input)-1])) input[strlen(input)-1] = '\0';
-
+    
     return S_OK;
 }
 
